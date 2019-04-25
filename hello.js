f14d413b6f7cfd2dcbb4fecb01cd9be5116b893e
ninjacoderboy <coderboy.contact@gmail.com>
2019-04-25 07:11:59 -0700

    Added an object to the hello.js file

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
 let im = 'Wow! Its amazing! For First time im writing code to push into Github';
 
 console.log(im);
+
+let me = {name:'Meer Estiyak', age = 16, Passion: 'Programming'};
+
+console.log(me)
